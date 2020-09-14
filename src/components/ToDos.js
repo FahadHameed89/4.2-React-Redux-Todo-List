a006ef07f6ce764aa7054d871c8e1a22d39e8f69
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-14 13:42:33 -0600

    Added connect() for our todo's components

--- src/components/ToDos.js
+++ src/components/ToDos.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 
-function ToDos () 
+function ToDos ( props ) 
 {
     const [newTask, setNewTask] = useState( '' );
 
@@ -24,11 +24,15 @@ function ToDos ()
             onChange= { event => {setNewTask( event.target.value); } } />
             <input value="Add New To-Do" type="submit" />
         </form>
-
+        <ul></ul>
         </>
     );
 }
 
-export default connect()(ToDos);
+export default connect(
+// We set up a 'mapStateToProps' function / relationship. 
+// Basically whatever is returned from Connect basically adds a props to 
+    state => { return { toDos: state } } 
+)(ToDos);
 
 // Line 16 - OnChange, We create a function for an event in which it sets newTask = event.target.value
