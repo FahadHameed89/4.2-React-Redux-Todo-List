59335790bb71e0816f23967e450286c6f5602e48
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-14 11:51:13 -0600

    Begin Redux Dispatch example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import toDosReducer from './reducers/todos';
+
+/**
+ * Redux Store
+ * Store is our global state. This iw where all of our global data exists. We pass in the reducer so it will know how to handle any actions (requests.)
+ */
+
+const store = createStore( toDosReducer );
+
+// Default method included in store called 'subscribe', which is used to run instructions every time there is an update to the store.
+// In this case we are requesting it to console log the store's state every time there is a change.  
+store.subscribe( () => console.log( store.getState() ) );
+
+/**
+ * Redux Dispatch
+ */
 
 ReactDOM.render(
   <React.StrictMode>
